794d4716ac0e60886ab46ade0a20a626fa090efa
vghryy <shuravlevkirill@gmail.com>
2025-01-08 14:32:31 +0600

    12

---
+++ lab1/Project4/Source.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    //1 задание
+
+    double x1 = 829.356;
+    double y1 = 128.286;
+
+    cout << "Задание 1" << endl;
+
+    cout << "1) " << x1 << " + " << y1 << " = " << x1 + y1 << endl;
+    cout << "2) " << scientific << setprecision(6) << x1 << endl;
+    cout << "3) " << scientific << setprecision(2) << y1 << endl;
+    cout << "4) " << setprecision(2) << fixed << x1 << endl;
+    cout << "5) " << setw(13) << setprecision(3) << fixed << y1 << endl;
+
+    //2 задание
+
+    int x2 = 2;
+    int y2 = 5;
+
+    cout << "Задание 2" << endl;
+
+    cout << "----------------------------------" << endl;
+    cout << "| № | Выражение     | Результат |" << endl;
+    cout << "----------------------------------" << endl;
+    cout << "| 1. | z=2/5         | " << setw(9) << 2 / 5 << " |" << endl;
+    cout << "| 2. | z=2./5        | " << setw(9) << 2. / 5 << " |" << endl;
+    cout << "| 3. | z=2/5.        | " << setw(9) << 2 / 5. << " |" << endl;
+    cout << "| 4. | z=x/5         | " << setw(9) << x2 / 5 << " |" << endl;
+    cout << "| 5. | z=x/5.        | " << setw(9) << x2 / 5. << " |" << endl;
+    cout << "| 6. | z=x/y         | " << setw(9) << x2 / y2 << " |" << endl;
+    cout << "| 7. | z=double(x)/5 | " << setw(9) << double(x2) / 5 << " |" << endl;
+    cout << "| 8. | z=x/double(y) | " << setw(9) << x2 / double(y2) << " |" << endl;
+    cout << "| 9. | z=2%5         | " << setw(9) << 2 % 5 << " |" << endl;
+    cout << "| 10 | z=y%x         | " << setw(9) << y2 % x2 << " |" << endl;
+    cout << "----------------------------------" << endl;
+
+    //3 задание
+
+    double x3, y3;
+    int position, sign;
+
+    cout << "Задание 3" << endl;
+    cout << "Введите x:" << endl;
+    cin >> x3;
+    cout << "Введите y:" << endl;
+    cin >> y3;
+    cout << "Введите количество позиций для вывода значения: " << endl;
+    cin >> position;
+    cout << "Введите количество отображаемых знаков в дробной части: " << endl;
+    cin >> sign;
+    cout.precision(sign);
+    cout << fixed << setw(position) << x3 + y3 << endl;
+    cout << fixed << setw(position) << x3 - y3 << endl;
+    cout << fixed << setw(position) << x3 * y3 << endl;
+    cout << fixed << setw(position) << x3 / y3 << endl;
+
+    //4 задание
+
+    string name;
+
+    cout << "Задание 4" << endl;
+    cout << "Введите свое имя: ";
+    cin >> name;
+    cout << setw(40) << "Здравствуйте, " << name;
+
+    return 0;
+}
---
+++ lab3/lab3/Source.cpp
@@ -0,0 +1,139 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+#include <cctype>   
+#include <clocale>  
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    //1 задание
+
+    double a, b, c;
+
+    cout << "Задание 1" << endl;
+    cout << "Введите 3 числа" << endl;
+    cin >> a >> b >> c;
+    if (a >= 0 && b >= 0 && c >= 0) {
+        cout << "Все числа положительные" << endl;
+    } else if (a < 0 && b < 0 && c < 0) {
+        cout << "Все числа отрицательные" << endl;
+    } else {
+        cout << "Не все числа положительные" << endl;
+    }
+
+    //2 задание
+
+    double a1, b1, c1;
+
+    cout << "Задание 2" << endl;
+    cout << "Введите 3 стороны" << endl;
+    cin >> a1 >> b1 >> c1;
+    if (a1 + b1 > c1 && a1 + c1 > b1 && b1 + c1 > a1) {
+        cout << "Треугольник существует" << endl;
+    } else {
+        cout << "Треугольник не существует" << endl;
+    }
+
+    //3 задание
+
+    double x1, y1, x2, y2, x3, y3, a2, b2, c2;
+
+    cout << "Задание 3" << endl;
+    cout << "Введите координаты" << endl;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    a2 = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+    b2 = sqrt((x2 - x3)*(x2 - x3) + (y2 - y3)*(y2 - y3));
+    c2 = sqrt((x3 - x1)*(x3 - x1) + (y3 - y1)*(y3 - y1));
+    if (a2 + b2 > c2 && a2 + c2 > b2 && b2 + c2 > a2) {
+        a2 = a2*a2;
+        b2 = b2*b2;
+        c2 = c2*c2;
+        if (a2 + b2 == c2 || a2 + c2 == b2 || b2 + c2 == a2) {
+            cout << "Треугольник прямоугольный" << endl;
+        } else if (a2 + b2 < c2 || a2 + c2 < b2 || b2 + c2 < a2) {
+            cout << "Треугольник тупоугольный" << endl;
+        } else {
+            cout << "Треугольник остроугольный" << endl;
+        }
+    } else {
+        cout << "Треугольник не существует" << endl;
+    }
+
+    //4 задание
+
+    double S;
+    int q, n, i = 1, b5 = 2, b12 = 2, b22 = 6;
+
+    cout << "Задание 4" << endl;
+    cout << "Введите количество членов последовательности ";
+    cin >> n;
+    q = b22/b12;
+    while (i < n) {
+        i++;
+        b5 *= q;
+    }
+    S = (b5*q - b12)/(q - 1);
+    cout << "Сумма первых " << n << " членов геометрической прогрессии равна " << S << endl;
+
+    //5 задание
+
+    int n1, j;
+    double S1 = 0.0;
+
+    cout << "Задание 5" << endl;
+    cout << "Введите n" << endl;
+    cin >> n1;
+    while (n1 <= 1 || n1 % 2 == 0) {
+        cout << "Введите верное n (нечетное и больше 1)" << endl;
+        cin >> n1;
+    }
+
+    for (j = 1; j < n1; j += 2) {
+        S1 += 1/(sqrt((j + 2)*j));
+    }
+    cout << "S = " << S1 << endl;
+
+    //6 задание
+
+    unsigned char ch;
+
+    cout << "Задание 6" << endl;
+    cout << "Введите символ: ";
+    cin >> ch;
+
+    if ((ch > 64 && ch < 91)||(ch > 96 && ch < 123)) {
+        cout << "Латинская буква" << endl;
+    } else if ((ch > 47)&&(ch < 58)) {
+        cout << "Цифра" << endl;
+    } else if ((ch > 31)&&(ch < 48)) {
+        cout << "Знак препинания" << endl;
+    } else if (((ch >= 0xC0)&&(ch <= 0xDF))||((ch >= 0xE0)&&(ch <= 0xFF))) {
+        cout << "Русская буква" << endl;
+    } else {
+        cout << "Прочий символ" << endl;
+    }
+
+    // 7 задание
+
+    int  n2, k;
+    string name, greeting;
+
+    cout << "Задание 7" << endl;
+    cout << "Введите имя" << endl;
+    cin >> name;
+    greeting = "Good afternoon, " + name + "!";
+    n2 = size(greeting) + 4;
+    for (int k = 0; k < n2; ++k) {
+        cout << "*";
+    }
+    cout << endl;
+    cout << "* " << greeting << " *" << endl;
+    for (int k = 0; k < n2; ++k) {
+        cout << "*";
+    }
+
+    return 0;
+}
